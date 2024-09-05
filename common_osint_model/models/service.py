a48b79194713908500338af83e6f144665918b21
Christoph Lobmeyer <lochr+gitcommit@posteo.de>
2024-09-05 21:09:05 +0200

    Handle non existing `observed_at` field in Censys result dictionary

--- common_osint_model/models/service.py
+++ common_osint_model/models/service.py
@@ -106,6 +106,7 @@ class Service(BaseModel, ShodanDataHandler, CensysDataHandler, BinaryEdgeDataHan
         if "dns" in d:
             dnsobj = DNSComponent.from_censys(d)
 
+        timestamp = datetime.fromisoformat(d["observed_at"][:-4]) if "observed_at" in d else None
         return Service(
             port=port,
             banner=banner,
